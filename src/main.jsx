f3217e6ef7577ec0ef8b23490f6153c4a2e9908e
sacm <sacm1046@gmail.com>
2025-04-01 20:37:34 -0300

    ready to deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Main from './components/main'
+import Actions from './components/actions'
 import './styles/index.scss'
 
 createRoot(document.getElementById('root')).render(
