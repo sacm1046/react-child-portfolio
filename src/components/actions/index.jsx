55b9dc1ac5dbc5fe3fa1d33fc6a54ea28a9a82ff
sacm <sacm1046@gmail.com>
2025-04-01 21:22:56 -0300

    styles defined

--- src/components/actions/index.jsx
+++ src/components/actions/index.jsx
@@ -1,8 +1,12 @@
+import { useStore } from "../../hooks/useStore";
 import { toggleTheme } from "../../store";
+import './index.scss';
 
 export default function Actions () {
-  return <div>
-    <h3>Theme Section</h3>
+  const { theme } = useStore()
+
+  return <div className={`actions__container actions__container--${theme}`}>
+    <h3>Action Section</h3>
     <button onClick={toggleTheme}>toggleTheme</button>
   </div>
 }
--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,9 +1,11 @@
 import { useStore } from "../../hooks/useStore";
+import './index.scss';
 
-export default function Main () {
+export default function Main ({title}) {
   const { theme } = useStore()
 
-  return <div>
-    <h3>Theme selection: {theme}</h3>
+  return <div className={`main__container main__container--${theme}`}>
+    <h3>Main selection: {title || 'not defined'}</h3>
+    <h5>Theme: {theme}</h5>
   </div>
 }
